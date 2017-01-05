3c7e42005f0181ba3268ddf2eae5e40b8d32c21b
ledfordjs <ledfordjs@gmail.com>
2017-01-04 21:32:51 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    encapsulation: ViewEncapsulation.None,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent { }
